d423f45a7abc93444ccae23998d4404aee1a8754
Mohamed Ali <mida@appstudio.pro>
2018-04-29 13:25:39 +0200

    new update

--- assets/js/inits/home-init.js
+++ assets/js/inits/home-init.js
@@ -4,38 +4,37 @@ $(window).load(function() {
     $('main').fadeIn();
 
 
-        // Add slideDown animation to Bootstrap dropdown when expanding.
-        $('.languages .dropdown').on('show.bs.dropdown', function() {
-            $(this).find('.dropdown-menu').first().stop(true, true).fadeIn();
-        });
+    // Add slideDown animation to Bootstrap dropdown when expanding.
+    $('.languages .dropdown').on('show.bs.dropdown', function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).fadeIn();
+    });
 
-        // Add slideUp animation to Bootstrap dropdown when collapsing.
-        $('.languages .dropdown').on('hide.bs.dropdown', function() {
-            $(this).find('.dropdown-menu').first().stop(true, true).fadeOut();
-        });
+    // Add slideUp animation to Bootstrap dropdown when collapsing.
+    $('.languages .dropdown').on('hide.bs.dropdown', function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).fadeOut();
+    });
 
 });
 
 
 
 // functional thing to make the spyscroll
-$('.button-spy').click(function(){
-    var target = $(this).attr( "data-link" );
-    $('html,body').scrollTo(target,900);
+$('.button-spy').click(function() {
+    var target = $(this).attr("data-link");
+    $('html,body').scrollTo(target, 900);
 })
 
 $(window).scroll(function() {
-    if($(window).scrollTop() > 100){
+    if ($(window).scrollTop() > 100) {
         $('.goTop').fadeIn();
-    }
-    else{
+    } else {
         $('.goTop').fadeOut();
     }
 })
-$('.goTop').click(function(){
-    $('html,body').scrollTo(0,900);
+$('.goTop').click(function() {
+    $('html,body').scrollTo(0, 900);
 })
 
 
 // Site Content Height
-$('.site-content').height($(window).height() - $('nav').outerHeight())
+// $('.site-content .home-banner').height($(window).height() - $('nav').outerHeight())
